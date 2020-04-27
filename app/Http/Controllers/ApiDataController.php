27bd4bd4ada77cba8518cbabef1757621e9d79bc
William Desportes <williamdes@wdes.fr>
2020-04-27 02:05:36 +0200

    feat: make most of the dashboard work

--- app/Http/Controllers/ApiDataController.php
+++ app/Http/Controllers/ApiDataController.php
@@ -6,6 +6,8 @@ use App\Models\Commit;
 use App\Models\IssueComment;
 use App\Models\PullRequest;
 use Illuminate\Support\Facades\View;
+use Illuminate\Http\Request;
+use Carbon\Carbon;
 
 class ApiDataController extends AbstractController
 {
@@ -14,12 +16,44 @@ class ApiDataController extends AbstractController
      * Get search page
      * @return mixed Actually a Illuminate\View\View but phpstan does not agree
      */
-    public function getSearchIndex()
+    public function getSearchIndex(Request $request)
     {
+        if ($request->query->has('searchTerm')) {
+            $searchDate = Carbon::createFromFormat('!Y-m-d', $request->query->get('searchDate'));
+            $searchTerm = $request->query->get('searchTerm');
+            $searchTermSql = '%' . $searchTerm . '%';
+            $commits = Commit::with('repository')->forDate($searchDate)->forTerm($searchTermSql);
+            $commitCount = $commits->count();
+            $issueCount = IssueComment::forDate($searchDate)->forTerm($searchTermSql)->count();
+            $pullsCount = PullRequest::forDate($searchDate)->forTerm($searchTermSql)->count();
+            return View::make('index', [
+                'eventCount' => $commitCount + $issueCount + $pullsCount,
+                'dateString' => $searchDate->format('d m Y'),
+                'searchDate' => $searchDate->format('Y-m-d'),
+                'searchTerm' => $searchTerm,
+                'commits' => $commits->limit(1000)->get(),
+                'commitsPoints' => [],
+                'pullsPoints' => [],
+                'commentsPoints' => [],
+                'commitCount' => $commitCount,
+                'commentCount' => $issueCount,
+                'pullsCount' => $pullsCount,
+            ]);
+        }
         return View::make('index', [
+            'searchDate' => $this->getSuggestedDate(),
             'commitCount' => Commit::count(),
             'commentCount' => IssueComment::count(),
             'pullsCount' => PullRequest::count(),
         ]);
+    }
+
+    private function getSuggestedDate(): string
+    {
+        $firstCommit = Commit::first();
+        if ($firstCommit === null) {
+            return Carbon::now()->format('Y-m-d');
+        }
+        return $firstCommit->{'created_at'}->format('Y-m-d');
     }
 }
--- tests/Feature/WelcomeTest.php
+++ tests/Feature/WelcomeTest.php
@@ -7,13 +7,37 @@ use Tests\TestCase;
 class ExampleTest extends TestCase
 {
     /**
-     * A basic test example.
+     * Test the the welcome page works
      *
      * @return void
      */
-    public function testBasicTest()
+    public function testWelcomePage(): void
     {
         $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+
+    /**
+     * Test the the search page works
+     *
+     * @return void
+     */
+    public function testSearchPage(): void
+    {
+        $response = $this->get('/search');
+
+        $response->assertStatus(200);
+    }
+
+    /**
+     * Test the the search page works with query params
+     *
+     * @return void
+     */
+    public function testSearchPageParams(): void
+    {
+        $response = $this->get('/search?searchDate=2020-04-26&searchTerm=l');
 
         $response->assertStatus(200);
     }
